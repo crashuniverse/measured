ce64cfbfaa021732f9c217fc5f1177c724d340e2
Priya Ranjan Singh <priyaranjan_singh@mckinsey.com>
2013-08-24 09:15:34 +0530

    adding success message

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,9 +3,13 @@
 angular.module('measuredApp')
 	.controller('MainCtrl', ['$scope', 'parsely',
 		function($scope, parsely) {
-			$scope.postMessage = function(weight) {
+			$scope.app = {};
+			$scope.app.input = 1;
+			$scope.app.postMessage = function(weight) {
 				parsely.postMessage(weight, function(data) {
 					console.log(data);
+					$scope.app.input = 0;
+					$scope.app.output = 1;
 				});
 			};
 		}
