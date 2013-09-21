5aa99bc36dbb7661206277c6458524eb1fd44a16
Priya Ranjan Singh <mail@priyaranjan.net>
2013-09-21 20:25:34 +0530

    writing test for chart directive

--- test/spec/directives/chart.js
+++ test/spec/directives/chart.js
@@ -13,8 +13,8 @@ describe('Directive: chart', function () {
   }));
 
   it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<chartjs></chartjs>');
+    element = angular.element('<div></div>');
     element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the chartjs directive');
+    expect(element.text()).toBe('');
   }));
 });
